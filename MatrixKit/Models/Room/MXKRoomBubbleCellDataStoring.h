ea6f4421826d8dd08cdb483f077dd373671a51c9
manuroe <manu@matrix.org>
2015-04-16 15:41:59 +0200

    MXKRoomBubbleCellDataStoring: Removed the design discussion about how to cut business logic between datasource and cellDataStoring. The option we chose seems to be the right one.

--- MatrixKit/Models/Room/MXKRoomBubbleCellDataStoring.h
+++ MatrixKit/Models/Room/MXKRoomBubbleCellDataStoring.h
@@ -25,28 +25,6 @@
 /**
  `MXKRoomBubbleCellDataStoring` defines a protocol a class must conform in order to store MXKRoomBubble cell data
  managed by `MXKRoomDataSource`.
- 
- @discussion
- @TODO: As it is currently implemented, MXKRoomDataSource passed each event to a MXKRoomBubbleCellDataStoring class object.
- This later, in initWithEvent or addEvent, will process the event in order to extract data to display in the cell.
- Not sure it is the right way.
-
- The other way is to keep all business logic in the MXKRoomDataSource and use MXKRoomBubbleCellDataStoring only
- as a storing class.
-
- Pros/Cons of the current implementation:
- Cons:
-   - These methods are called on the internal processing queue of MXKRoomDataSource.
-   - The business logic risks to be cut in 2 classes the MXKRoomBubbleCellDataStoring class and MXKRoomDataSource.
- 
- Pros:
-   - This model seems easier for developers wanting to customize the display.
-     The developer creates his own MXKCellRendering class for room bubbles display. If he needs more data, he can
-     can create his own MXKRoomBubbleCellDataStoring class that will be able to extract the required data.
-     There is no need to change code in MXKRoomDataSource.
- 
- For now, we keep the current design. When the MXKRoomViewController will be able to display messages as Console does, 
- we will check if we need to change the implementation.
  */
 @protocol MXKRoomBubbleCellDataStoring <NSObject>
 
