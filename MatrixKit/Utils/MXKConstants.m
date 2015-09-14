5735c6d066864e5e7d8bee5595db0405ea06f8d0
giomfo <giom@matrix.org>
2015-09-14 10:57:31 +0200

    Update version number: 0.2.3

--- MatrixKit/Utils/MXKConstants.m
+++ MatrixKit/Utils/MXKConstants.m
@@ -16,6 +16,6 @@
 
 #import "MXKConstants.h"
 
-NSString *const MatrixKitVersion = @"0.2.2";
+NSString *const MatrixKitVersion = @"0.2.3";
 
 NSString *const kMXKErrorNotification = @"kMXKErrorNotification";
