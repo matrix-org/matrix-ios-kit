a39c2ec73686525d334822384049455ca88dde52
yannick <yannick@matrix.org>
2015-12-15 18:12:34 +0100

    Add more events to listen

--- MatrixKit/Controllers/MXKRoomSettingsViewController.m
+++ MatrixKit/Controllers/MXKRoomSettingsViewController.m
@@ -60,7 +60,7 @@
     if (mxRoom)
     {
         // Register a listener to handle messages related to room name, topic...
-        roomListener = [mxRoom listenToEventsOfTypes:@[kMXEventTypeStringRoomName, kMXEventTypeStringRoomAliases, kMXEventTypeStringRoomMember]
+        roomListener = [mxRoom listenToEventsOfTypes:@[kMXEventTypeStringRoomName, kMXEventTypeStringRoomAliases, kMXEventTypeStringRoomMember, kMXEventTypeStringRoomAvatar, kMXEventTypeStringRoomPowerLevels, kMXEventTypeStringRoomCanonicalAlias]
                                              onEvent:^(MXEvent *event, MXEventDirection direction, MXRoomState *roomState)
                         {
                             // Consider only live events
