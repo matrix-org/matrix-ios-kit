2fa1589e0f07d48088cd77fdcd3a1345d74987e7
giomfo <giom@matrix.org>
2016-01-11 11:37:51 +0100

    Bug Fix - App freezes during back pagination in #matrix-spam.
    We limit the number of components to reduce the computation time required during bubble handling.
    Indeed some process like [prepareBubbleComponentsPosition] is time consuming.

--- MatrixKit/Models/Room/MXKRoomBubbleCellDataWithAppendingMode.m
+++ MatrixKit/Models/Room/MXKRoomBubbleCellDataWithAppendingMode.m
@@ -22,10 +22,22 @@ static NSAttributedString *messageSeparator = nil;
 
 #pragma mark - MXKRoomBubbleCellDataStoring
 
+- (instancetype)initWithEvent:(MXEvent *)event andRoomState:(MXRoomState *)roomState andRoomDataSource:(MXKRoomDataSource *)roomDataSource2
+{
+    self = [super initWithEvent:event andRoomState:roomState andRoomDataSource:roomDataSource2];
+    if (self)
+    {
+        // Set default settings
+        self.maxComponentCount = 10;
+    }
+    
+    return self;
+}
+
 - (BOOL)addEvent:(MXEvent*)event andRoomState:(MXRoomState*)roomState
 {
     // We group together text messages from the same user (attachments are not merged).
-    if ([event.sender isEqualToString:self.senderId] && (self.attachment == nil))
+    if ([event.sender isEqualToString:self.senderId] && (self.attachment == nil) && (self.bubbleComponents.count < self.maxComponentCount))
     {
         // Attachments (image, video ...) cannot be added here
         if ([roomDataSource.eventFormatter isSupportedAttachment:event])
