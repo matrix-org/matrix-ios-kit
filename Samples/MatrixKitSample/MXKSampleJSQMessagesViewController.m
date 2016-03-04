7e8efd313efd80edfe34bceee2e6524dff970c20
manuroe <manu@matrix.org>
2016-03-04 16:12:50 +0100

    Context timeline: Fixed compilation in JSQMessages

--- Samples/MatrixKitSample/MXKSampleJSQMessagesViewController.m
+++ Samples/MatrixKitSample/MXKSampleJSQMessagesViewController.m
@@ -95,7 +95,7 @@
     membersPlaceHolderAvatar = [NSMutableDictionary dictionary];
     
     // Start showing history right now
-    [roomDataSource paginateBackMessagesToFillRect:self.view.frame withMinRequestMessagesCount:30 success:^{
+    [roomDataSource paginateToFillRect:self.view.frame direction:MXTimelineDirectionBackwards  withMinRequestMessagesCount:30 success:^{
         // @TODO (hide loading wheel)
     } failure:^(NSError *error)
     {
@@ -491,7 +491,7 @@
 {
     // Disable the button while requesting
     sender.enabled = NO;
-    [roomDataSource paginateBackMessages:30 onlyFromStore:NO success:^(NSUInteger addedCellNumber){
+    [roomDataSource paginate:30 direction:MXTimelineDirectionBackwards onlyFromStore:NO success:^(NSUInteger addedCellNumber){
         
         // Pagingate messages have been received by the didChange protocol
         // Just need  to reenable the button
