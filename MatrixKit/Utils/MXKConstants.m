66e9b46fca9adf33df0e130fdb28a3fc5137bc62
giomfo <giom@matrix.org>
2016-04-26 17:13:42 +0200

    Prepare matrix-ios-kit release v0.3.6

--- MatrixKit/Utils/MXKConstants.m
+++ MatrixKit/Utils/MXKConstants.m
@@ -16,6 +16,6 @@
 
 #import "MXKConstants.h"
 
-NSString *const MatrixKitVersion = @"0.3.5";
+NSString *const MatrixKitVersion = @"0.3.6";
 
 NSString *const kMXKErrorNotification = @"kMXKErrorNotification";
