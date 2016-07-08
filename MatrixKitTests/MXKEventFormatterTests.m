57f9c99a512a8fe61418771d197f7d551c03373a
manuroe <manu@matrix.org>
2016-07-08 14:22:17 +0200

    MXKEventFormatter: Started unit testing

--- MatrixKitTests/MXKEventFormatterTests.m
+++ MatrixKitTests/MXKEventFormatterTests.m
@@ -1,18 +1,25 @@
-//
-//  MXKEventFormaterTests.m
-//  MatrixKit
-//
-//  Created by Emmanuel ROHEE on 07/07/16.
-//  Copyright © 2016 matrix.org. All rights reserved.
-//
+/*
+ Copyright 2016 OpenMarket Ltd
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 
 #import <XCTest/XCTest.h>
 
 #import "MatrixKit.h"
 
-
-
-@interface MXEventFormaterTests : XCTestCase
+@interface MXEventFormatterTests : XCTestCase
 {
     MXKEventFormatter *eventFormatter;
     MXEvent *anEvent;
@@ -20,12 +27,14 @@
 
 @end
 
-@implementation MXEventFormaterTests
+@implementation MXEventFormatterTests
 
 - (void)setUp
 {
     [super setUp];
 
+    // Create a minimal event formatter
+    // Note: it may not be enough for testing all MXKEventFormatter methods
     eventFormatter = [[MXKEventFormatter alloc] initWithMatrixSession:nil];
     anEvent = [eventFormatter fakeRoomMessageEventForRoomId:@"aRoomId"
                                                 withEventId:@"anEventId"
@@ -40,7 +49,7 @@
     [super tearDown];
 }
 
-- (void)testMarkDowmToHtmlPreCode
+- (void)testRenderHTMLStringWithPreCode
 {
     NSString *html = @"<pre><code>1\n2\n3\n4\n</code></pre>";
     NSAttributedString *as = [eventFormatter renderHTMLString:html forEvent:anEvent];
@@ -49,13 +58,11 @@
 
     // \R : any newlines
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\\R" options:0 error:0];
-
-    XCTAssertEqual(3, [regex numberOfMatchesInString:a options:0 range:NSMakeRange(0, a.length)], "There must be no line wrapping in <pre> blocks");
+    XCTAssertEqual(3, [regex numberOfMatchesInString:a options:0 range:NSMakeRange(0, a.length)], "renderHTMLString must keep line break in <pre> and <code> blocks");
 
     [as enumerateAttributesInRange:NSMakeRange(0, as.length) options:(0) usingBlock:^(NSDictionary<NSString *,id> * _Nonnull attrs, NSRange range, BOOL * _Nonnull stop) {
 
         UIFont *font = attrs[NSFontAttributeName];
-
         XCTAssertEqualObjects(font.fontName, @"Courier", "The font for <pre> and <code> should be monospace");
     }];
 
