27e48fd3f9113413486a4e198cebc676dd1e4133
manuroe <manu@matrix.org>
2016-07-26 11:49:02 +0200

    Added use-dev-pods.sh to help Jenkins to build develop version

--- use-dev-pods.sh
+++ use-dev-pods.sh
@@ -3,8 +3,10 @@
 # This script modifies Podfile in order to use Matrix pods on their develop branch.
 # It is intended to be used by Jenkins to build the develop version of the app.
 
+echo Moving Podfile to develop Matrix pods
+
 # Podfile.lock will be obsolete reset it 
-rm Podfile.lock
+rm -f Podfile.lock
 
 # Disable the active pod
 sed -i '' -E "s!^(pod)(.*MatrixSDK)!#\1\2!g" Podfile
