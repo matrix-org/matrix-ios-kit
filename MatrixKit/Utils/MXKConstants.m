dc6ec56c0bb5191401dd4b8e6e5ff55a6ff59e88
giomfo <giom@matrix.org>
2016-11-17 17:40:08 +0100

    Upgrade matrixKit version: 0.4.0

--- MatrixKit/Utils/MXKConstants.m
+++ MatrixKit/Utils/MXKConstants.m
@@ -16,6 +16,6 @@
 
 #import "MXKConstants.h"
 
-NSString *const MatrixKitVersion = @"0.3.19";
+NSString *const MatrixKitVersion = @"0.4.0";
 
 NSString *const kMXKErrorNotification = @"kMXKErrorNotification";
