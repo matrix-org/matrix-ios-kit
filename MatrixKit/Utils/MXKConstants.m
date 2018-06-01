b0ef77b6fa7d0224568f58fa0468dc1da8c0dfa8
manuroe <manu@matrix.org>
2018-06-01 16:41:09 +0200

    version++

--- MatrixKit/Utils/MXKConstants.m
+++ MatrixKit/Utils/MXKConstants.m
@@ -17,7 +17,7 @@
 
 #import "MXKConstants.h"
 
-NSString *const MatrixKitVersion = @"0.7.13";
+NSString *const MatrixKitVersion = @"0.7.14";
 
 NSString *const kMXKErrorNotification = @"kMXKErrorNotification";
 
