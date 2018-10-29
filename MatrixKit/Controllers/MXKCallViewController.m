matrix-org/matrix-ios-kit

commit b6010fa26b7920f4fadc3a91badd8cc8c9a5aff2

Lazy-loading server side: Use MXRoomState.members instead of [MXRoom members:] when possible

MatrixKit/Controllers/MXKCallViewController.m
@@ -783,11 +783,11 @@ NSString *const kMXKCallViewControllerBackToAppNotification = @"kMXKCallViewCont
         if (!mxCall.isConferenceCall)
         {
             MXWeakify(self);
-            [mxCall.room members:^(MXRoomMembers *roomMembers) {
+            [mxCall.room state:^(MXRoomState *roomState) {
                 MXStrongifyAndReturnIfNil(self);
             
                 MXUser *theMember = nil;
-                NSArray *members = roomMembers.joinedMembers;
+                NSArray *members = roomState.members.joinedMembers;
                 for (MXUser *member in members)
                 {
                     if (![member.userId isEqualToString:self->mxCall.callerId])

MatrixKit/Models/Contact/MXKContactManager.m
@@ -210,11 +210,13 @@ NSString *const kMXKContactManagerDidInternationalizeNotification = @"kMXKContac
                 
                 MXStrongifyAndReturnIfNil(self);
                 
-                // create contact for room members
+                // create contact for known room members
                 if (self.contactManagerMXRoomSource != MXKContactManagerMXRoomSourceNone)
                 {
                     MXRoom *room = notif.object;
-                    [room members:^(MXRoomMembers *roomMembers) {
+                    [room state:^(MXRoomState *roomState) {
+
+                        MXRoomMembers *roomMembers = roomState.members;
 
                         NSArray *members = roomMembers.members;
 

commit 13a4ec2bccc4a58cd8760c573c83cd35c8b55235

MXKCallViewController: Fix crash in callRoomStateDidChange
https://github.com/vector-im/riot-ios/issues/2079

Regression made with LL in commit b6010fa2.

MatrixKit/Controllers/MXKCallViewController.m
@@ -778,7 +778,10 @@ NSString *const kMXKCallViewControllerBackToAppNotification = @"kMXKCallViewCont
     if (mxCall.isIncoming)
     {
         self.peer = [mxCall.room.mxSession getOrCreateUser:mxCall.callerId];
-        onComplete();
+        if (onComplete)
+        {
+            onComplete();
+        }
     }
     else
     {
@@ -802,13 +805,19 @@ NSString *const kMXKCallViewControllerBackToAppNotification = @"kMXKCallViewCont
                 }
 
                 self.peer = theMember;
-                onComplete();
+                if (onComplete)
+                {
+                    onComplete();
+                }
             }];
         }
         else
         {
             self.peer = nil;
-            onComplete();
+            if (onComplete)
+            {
+                onComplete();
+            }
         }
     }
 }