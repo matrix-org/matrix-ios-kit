8c6ac9b597d7bfcea876004d85c61c58ea954b6b
manuroe <manu@matrix.org>
2019-07-16 07:59:07 +0200

    BF: Push notifications are spontaneously disabling themselves
    https://github.com/vector-im/riot-ios/issues/2348
    
    Do not reset MXKAccount.enablePushKitNotifications when trying to refresh pushkit pusher at startup.
    If the requests fails, the app will consider that push is disabled whereas there is still a pusher on the homeserver.
    
    This is visible at https://github.com/matrix-org/riot-ios-rageshakes/issues/2618 where the user gets pushes whereas the app setting says there are disabled.
    
    Old logs of https://github.com/matrix-org/riot-ios-rageshakes/issues/2615 show the same thing but on the last app update, the user lost pushes probably because of lack of refresh of the pusher because MXKAccount.enablePushKitNotifications was NO for a long time.

--- MatrixKit/Models/Account/MXKAccount.m
+++ MatrixKit/Models/Account/MXKAccount.m
@@ -2,6 +2,7 @@
  Copyright 2015 OpenMarket Ltd
  Copyright 2017 Vector Creations Ltd
  Copyright 2018 New Vector Ltd
+ Copyright 2019 The Matrix.org Foundation C.I.C
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -1133,11 +1134,7 @@ static MXKAccountOnCertificateChange _onCertificateChangeBlock;
         [self enablePushKitPusher:YES
                           success:nil
                           failure:^(NSError *error) {
-                              
-                              self->_enablePushKitNotifications = NO;
-                              
-                              // Archive updated field
-                              [[MXKAccountManager sharedManager] saveAccounts];
+                              NSLog(@"[MXKAccount][Push] refreshPushKitPusher: Error: %@", error);
                           }];
     }
     else if (_enablePushKitNotifications && mxSession)
