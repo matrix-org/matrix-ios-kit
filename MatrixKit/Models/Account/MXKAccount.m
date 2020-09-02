6dd4d9ce31fd1808b53ec9a5e064ccff4ae93f41
manuroe <manu@matrix.org>
2020-09-02 15:47:26 +0200

    MXKAccount: enablePushKitNotifications: Make sure we call the callback in all cases

--- MatrixKit/Models/Account/MXKAccount.m
+++ MatrixKit/Models/Account/MXKAccount.m
@@ -534,6 +534,11 @@ static MXKAccountOnCertificateChange _onCertificateChangeBlock;
             }
         }];
     }
+    else
+    {
+        NSLog(@"[MXKAccount][Push] enablePushKitNotifications: PushKit is already disabled for %@", self.mxCredentials.userId);
+        success();
+    }
 }
 
 - (void)setEnableInAppNotifications:(BOOL)enableInAppNotifications
