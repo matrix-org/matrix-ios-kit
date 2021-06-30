635cf32df2384b4b36bdc9591485eb5a3bdb1148
ismailgulek <ismailg@matrix.org>
2021-06-30 17:58:43 +0300

    Move state logic into an extension

---
+++ MatrixKit/Categories/MXSession.swift
@@ -0,0 +1,33 @@
+// 
+// Copyright 2020 The Matrix.org Foundation C.I.C
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+import MatrixSDK
+
+@objc
+extension MXSession {
+    
+    /// Flag to indicate whether the session is in a suitable state to show some activity indicators on UI.
+    var shouldShowActivityIndicator: Bool {
+        switch state {
+        case .initialised, .processingLocalCache, .syncInProgress:
+            return true
+        default:
+            return false
+        }
+    }
+    
+}
