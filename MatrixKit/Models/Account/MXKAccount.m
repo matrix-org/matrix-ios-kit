dd98474a12e2713ae30687581725095447960963
Doug <douglase@element.io>
2021-08-10 11:13:19 +0100

    Update the default notification payload.

--- MatrixKit/Models/Account/MXKAccount.m
+++ MatrixKit/Models/Account/MXKAccount.m
@@ -1244,7 +1244,7 @@ static NSArray<NSNumber*> *initialSyncSilentErrorsHTTPStatusCodes;
     NSDictionary *pushData = @{
         @"url": self.pushGatewayURL,
         @"format": @"event_id_only",
-        @"default_payload": @{@"aps": @{@"mutable-content": @(1), @"alert": @{@"loc-key": @"MESSAGE", @"loc-args": @[]}}}
+        @"default_payload": @{@"aps": @{@"mutable-content": @(1), @"alert": @{@"loc-key": @"NOTIFICATION", @"loc-args": @[]}}}
     };
     
     [self enablePusher:enabled appId:appId token:[MXKAccountManager sharedManager].apnsDeviceToken pushData:pushData success:^{
