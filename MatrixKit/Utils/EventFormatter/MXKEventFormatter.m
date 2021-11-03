b5ebd0336b44d86ed810be10056b2e110839edab
ismailgulek <ismailg@matrix.org>
2021-11-04 01:57:42 +0300

    Support in-thread replies

--- MatrixKit/Utils/EventFormatter/MXKEventFormatter.m
+++ MatrixKit/Utils/EventFormatter/MXKEventFormatter.m
@@ -1252,30 +1252,12 @@ static NSString *const kHTMLATagRegexPattern = @"<a href=\"(.*?)\">([^<]*)</a>";
 
                 NSString *body;
                 BOOL isHTML = NO;
-                NSString *eventThreadIdentifier = event.threadIdentifier;
 
                 // Use the HTML formatted string if provided
                 if ([event.content[@"format"] isEqualToString:kMXRoomMessageFormatHTML])
                 {
                     isHTML =YES;
                     MXJSONModelSetString(body, event.content[@"formatted_body"]);
-                }
-                else if (eventThreadIdentifier)
-                {
-                    isHTML = YES;
-                    MXJSONModelSetString(body, event.content[@"body"]);
-                    MXEvent *threadRootEvent = [mxSession.store eventWithEventId:eventThreadIdentifier
-                                                                          inRoom:event.roomId];
-                    
-                    NSString *threadRootEventContent;
-                    MXJSONModelSetString(threadRootEventContent, threadRootEvent.content[@"body"]);
-                    body = [NSString stringWithFormat:@"<mx-reply><blockquote><a href=\"%@\">In reply to</a> <a href=\"%@\">%@</a><br>%@</blockquote></mx-reply>%@",
-                            [MXTools permalinkToEvent:eventThreadIdentifier inRoom:event.roomId],
-                            [MXTools permalinkToUserWithUserId:threadRootEvent.sender],
-                            threadRootEvent.sender,
-                            threadRootEventContent,
-                            body];
-                    
                 }
                 else
                 {
@@ -1378,7 +1360,7 @@ static NSString *const kHTMLATagRegexPattern = @"<a href=\"(.*?)\">([^<]*)</a>";
                         // This helps us insert the emote prefix in the right place
                         NSDictionary *relatesTo;
                         MXJSONModelSetDictionary(relatesTo, event.content[@"m.relates_to"]);
-                        if ([relatesTo[@"m.in_reply_to"] isKindOfClass:NSDictionary.class] || event.isInThread)
+                        if ([relatesTo[@"m.in_reply_to"] isKindOfClass:NSDictionary.class])
                         {
                             [attributedDisplayText enumerateAttribute:kMXKToolsBlockquoteMarkAttribute
                                                               inRange:NSMakeRange(0, attributedDisplayText.length)
@@ -1712,7 +1694,7 @@ static NSString *const kHTMLATagRegexPattern = @"<a href=\"(.*?)\">([^<]*)</a>";
     NSString *html = htmlString;
 
     // Special treatment for "In reply to" message
-    if (event.isReplyEvent || event.isInThread)
+    if (event.isReplyEvent)
     {
         html = [self renderReplyTo:html withRoomState:roomState];
     }
@@ -2039,7 +2021,7 @@ static NSString *const kHTMLATagRegexPattern = @"<a href=\"(.*?)\">([^<]*)</a>";
         textColor = _errorTextColor;
     }
     // Check whether the message is highlighted.
-    else if (event.mxkIsHighlighted || (event.isInThread && ![event.sender isEqualToString:mxSession.myUserId]))
+    else if (event.mxkIsHighlighted)
     {
         textColor = _bingTextColor;
     }
@@ -2103,7 +2085,7 @@ static NSString *const kHTMLATagRegexPattern = @"<a href=\"(.*?)\">([^<]*)</a>";
     {
         font = _callNoticesTextFont;
     }
-    else if (event.mxkIsHighlighted || (event.isInThread && ![event.sender isEqualToString:mxSession.myUserId]))
+    else if (event.mxkIsHighlighted)
     {
         font = _bingTextFont;
     }
